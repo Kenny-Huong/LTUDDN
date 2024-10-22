bd80bf58a19bff995540041cbc642e81227aa2e8
Kenny-Huong <nguyenvanhuong892003@gmail.com>
2024-10-22 22:54:49 +0700

    init projetc

---
+++ NguyenVanHuong_BTVN_Tuan3/NguyenVanHuong_BTVN_Tuan3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenVanHuong_BTVN_Tuan3.MvcApplication" Language="C#" %>
---
+++ NguyenVanHuong_BTVN_Tuan3/NguyenVanHuong_BTVN_Tuan3/Models/SinhVien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NguyenVanHuong_BTVN_Tuan3.Models
+{
+    public class SinhVien
+    {
+        public string MaSinhVien {  get; set; }
+        public string HoTen {  get; set; }
+
+        public string NgaySinh { get; set; }
+        public string GioiTinh {  get; set; }
+        public string QueQuan {  get; set; }
+        public string LopHoc {  get; set; }
+
+    }
+}
